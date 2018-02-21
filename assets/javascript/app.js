6a1954bbbdd639f3dde83b3975ae8cd031ef728f
morgan crosta <morgancrosta@gmail.com>
2018-02-21 10:33:03 -0800

    timer needs work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,82 +1,118 @@
 //1. variables: questions[will these be objects?], timer, correctAnswers, incorrectAnswers, unanswered
-var questions = [
-	{
-		question: "When hippos are upset, what color does their sweat turn?",
-		answers: ["Blue", "Green", "Red", "Yellow"],
-		correctAnswer: "Red"
-	},
-	{	
-		question: "How many calories would you burn if you banged your head against a wall for one hour?",
-		answers: ["20", "60", "120", "150"],
-		correctAnswer: "150"
+var questions = [{
+        question: "When hippos are upset, what color does their sweat turn?",
+        answers: ["Blue", "Green", "Red", "Yellow"],
+        correctAnswer: "Red"
+    }, {
+        question: "How many calories would you burn if you banged your head against a wall for one hour?",
+        answers: ["20", "60", "120", "150"],
+        correctAnswer: "150"
 
-	},
-	{
-		question: "Fill in the blank: A sheep, a duck and a ___ were the first passengers on a hot air balloon.",
-		answers: ["Rooster", "Human", "Pig", "Sandwich"],
-		correctAnswer: "True"
-	},
-	{
-		question: "Paraskavedekatriaphobia is the fear of what?",
-		answers: ["Fun", "Friday the 13th", "Parasailing", "Carving a Turkey"],
-		correctAnswer: "Friday the 13th"
-	}
+    }, {
+        question: "Fill in the blank: A sheep, a duck and a ___ were the first passengers on a hot air balloon.",
+        answers: ["Rooster", "Human", "Pig", "Sandwich"],
+        correctAnswer: "Rooster"
+    }, {
+        question: "Paraskavedekatriaphobia is the fear of what?",
+        answers: ["Fun", "Friday the 13th", "Parasailing", "Carving a Turkey"],
+        correctAnswer: "Friday the 13th"
+    }
 
-] 
+]
 
 var i = 0;
+var intervalIndentity;
 
 var correctAnswers = 0;
 var incorrectAnswers = 0;
 var unansweredQuestions = 0;
 
+$("body").on('click', '.answer', function() {
+    console.log($(this).attr("options"));
+    console.log($(this).attr("correct"));
+
+    var userChoice = $(this).attr("options");
+    var correct = $(this).attr("correct");
+
+    checkAnswer(userChoice, correct)
+});
+
+
 function startGame() {
-	$("#youReady").empty();
-	qAs()
-	timer()
+    $("#youReady").empty();
+    qAs()
+        
 }
 // 2. create timer that will count down from 30
 var counter = 10;
+
 function timer() {
-	setInterval(function(){
-	  console.log(counter);
-	  counter--
-	  if (counter === 0) {
-	    console.log("derrrrrp");
-	   	counter = 10;
-	  }
-	}, 1000);
+    intervalIndentity = setInterval(function() {
+        console.log(counter);
+        counter--
+        $("#timer").html(counter);
+        if (counter === 0) {
+            console.log("derrrrrp");
+            clearInterval(intervalIndentity);
+            counter = 10;
+        }
+    }, 1000);
 }
- 
+
 
 //3. for loop that will display a question and the corresponding answers
 function qAs() {
+    $("#youReady").empty();
+    $("#answers").empty();
+    timer();
+    $("#question").html(questions[i].question);
 
-	$("#question").append(questions[i].question);
-	
-	for (var j = 0; j < questions.length; j++) {
-		
-		console.log(questions[i].question);
-		console.log(questions[i].answers);
-		console.log(questions[i].correctAnswer);
-		
-		var a = $("<button>");
-		a.addClass("answer");
-		a.text(questions[i].answers[j]);
-		$("#answers").append(a);
+    for (var j = 0; j < questions.length; j++) {
 
-		
-	}
 
-	//4.2 IF user runs out of time, display INCORRECT, log info into unanswered, move on to next question
-	
-// function checkAnswers() {
-// var userChoice = [];
-// 		if (userChoice === questions[i].correctAnswer) {
-// 			alert("YOU GOT IT!");
-// 		}
-// 	}
+        var a = $("<button>");
+        a.addClass("answer");
+        a.attr("options", questions[i].answers[j]);
+        a.attr("correct", questions[i].correctAnswer);
+        a.text(questions[i].answers[j]);
+        $("#answers").append(a);
+
+
+    }
+    console.log(questions[i].question);
+    console.log(questions[i].answers);
+    console.log(questions[i].correctAnswer);
+
 }
+
+function checkAnswer(userC, correctAnswer) {
+    if (counter == 0) {
+    	alert("you ran out of time dummy!");
+    	i++;
+    	unansweredQuestions++;
+    	console.log(unansweredQuestions);
+    }
+
+    else if (userC !== correctAnswer) {
+        alert("ding dong");
+        i++;
+        incorrectAnswers++;
+        console.log("incorrect answers" + incorrectAnswers);
+    } 
+
+    else if (userC === correctAnswer) {
+        alert("nailed it");
+        i++;
+        correctAnswers++;
+        console.log("correct answers" + correctAnswers);
+    }
+
+    qAs();
+}
+
+
+
+//4.2 IF user runs out of time, display INCORRECT, log info into unanswered, move on to next question
 
 
 //4.1 ELSE IF user clicks incorrect answer, display INCORRECT, log info into incorrectAnswers, move on to next question
